77ece6d6025c4897ba96933b7f229b27154b6aca
HOUSNI Mohamed <mohamed.housni@um5s.net.ma>
2021-05-05 21:33:38 +0000

    ic

---
+++ 1_findTarget.py
@@ -0,0 +1,40 @@
+
+import math
+
+"""
+Linear runtime: O(n)
+algo: linear search
+"""
+def findTargetLinear(list,target):
+  """
+  Return index of target if exists or None if not
+  """
+  for i in range(0,len(list)):
+    if(list[i]==target):
+      return i
+  return None
+
+"""
+Logarithmic runtime: O(log n )
+algo: binary search
+"""
+def fintTargetLog(list,target):
+  """
+  Assuming list is sorted
+  """
+  first = 0
+  last = len(list)-1
+  while first<last:
+    middle = math.floor((first+last)/2)
+    if list[middle]>target:
+      last = middle-1
+    elif list[middle]<target:
+      first = middle+1
+  if list[first]==target:
+    return first
+  else:
+    return None
+
+list = [1,2,3,4,5,6,7,8]
+target = 3
+print(fintTargetLog(list,target))
---
+++ README.md
@@ -0,0 +1,3 @@
+# Algorithms
+1. findTarget
+Given a list of integers and a target integer this function will return the index of the target if it exists or None (null in python)
