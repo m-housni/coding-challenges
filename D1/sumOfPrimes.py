3876882dbd08b0dd3fe4373cfc5c518e915be0e6
HOUSNI Mohamed <mohamed.housni@um5s.net.ma>
2021-05-09 12:30:47 +0000

    corrected python range last index issue

--- D1/sumOfPrimes.py
+++ D1/sumOfPrimes.py
@@ -5,16 +5,31 @@ Summation of primes
 * 
 N=100000 
 Result: 454396537
+N=2000000
+Result: 142913828922
+
+if n =  pxq 
+p<q
+p2<pq
+p2<n
+p<Vn
+
+example 
+6=3x2  => 2<V6
+9=3x3 => 3<V9
+26=2x13 => 2<V26
+
 """
+import math
 
 # is given numpber prime ?
 def isPrime(n):
-  for i in range(2, n//2):
+  for i in range(2, math.ceil(math.sqrt(n))+1):
     if(n % i == 0):
       return False
   return True
 
-# sum of primes less than 1000000
+# sum of primes less than N
 def sumOfPrimes(N):
   oddNum = 3
   sum = 2
@@ -23,6 +38,7 @@ def sumOfPrimes(N):
       sum += oddNum
     oddNum += 2
   return sum
+
 
 print(sumOfPrimes(100000))
 
